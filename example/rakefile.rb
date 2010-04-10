f8e89c1cf9a60b70e4f81055048ae0c6fe9454aa
oliver <oliver.mueller@gmail.com>
2010-04-10 16:36:40 +0200

    easy way to execute all example rake tasks and clean up again

--- example/rakefile.rb
+++ example/rakefile.rb
@@ -1,13 +1,21 @@
 
+desc 'clean all examples'
+task :clean_all do
+  run_rakefiles("rake clean")
+end
+
 desc 'build all examples'
 task :all_examples do
-  Dir.glob('**/Rakefile.rb').each do |p|
-    dir = File.dirname(p)
-    cd dir do
-      puts "running rake in #{dir}"
-      sh "rake"
-    end
-  end
+  run_rakefiles("rake")
 end
 
 task :default => :all_examples
+
+def run_rakefiles(c)
+  Dir.glob('**/Rakefile.rb').each do |p|
+    dir = File.dirname(p)
+    cd dir do
+      sh "#{c}"
+    end
+  end
+end
