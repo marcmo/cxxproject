3463b3546816b76db6970d22f429ffbeb37ef450
oliver <oliver.mueller@gmail.com>
2010-04-11 14:35:57 +0200

    tried to add ability to execute all built executables...not yet working...why?

--- example/Rakefile.rb
+++ example/Rakefile.rb
@@ -1,21 +1,32 @@
 desc 'clean all examples'
 task :clean_all do
-  run_rakefiles("rake clobber")
+  run_rakefiles{ sh "rake clobber" }
 end
 
 desc 'build all examples'
 task :all_examples do
-  run_rakefiles("rake")
+  run_rakefiles { sh "rake" }
 end
 
+desc "run all exes - task"
+task :runexes do
+  run_rakefiles do
+    if Rake::Task.task_defined?(:run)
+      Rake::Task.invoke(:run)
+    else
+      p "no run task exists in #{`pwd`}, only those tasks exist:"
+      p Rake::Task.tasks.collect {|t| t.name}
+    end
+  end
+end
 task :default => :all_examples
 
-def run_rakefiles(c)
+def run_rakefiles()
   Dir.glob('**/Rakefile.rb').each do |p|
     dir = File.dirname(p)
     if (dir != ".")
       FileUtils.cd(dir,:verbose => false)  do
-        sh "#{c}"
+        yield
       end
     end
   end
--- example/three_tests/Rakefile.rb
+++ example/three_tests/Rakefile.rb
@@ -1,2 +1,12 @@
 require 'cxxproject'
-CxxProject2Rake.new(Dir.glob('**/*project.rb'),OsxCompiler.new('build'))
+BuildDir='build'
+CxxProject2Rake.new(Dir.glob('**/*project.rb'),OsxCompiler.new(BuildDir))
+
+desc "run test exes"
+task :run do
+  exes = Dir.glob("**/*.exe").find_all{|f| File.executable?(f)}
+  exes.each do |x|
+    sh x
+  end
+end
+
