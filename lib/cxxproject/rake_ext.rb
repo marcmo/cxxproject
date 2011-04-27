01f9c8f0e4942deb041372bb93a739626e26af3b
alexander schaal <alex_cxxproject@cxxproject.com>
2011-04-27 09:53:17 +0200

    better thread handling for multitask

--- lib/cxxproject/rake_ext.rb
+++ lib/cxxproject/rake_ext.rb
@@ -2,17 +2,43 @@ require 'rake'
 
 module Rake
 
-  $queue = Queue.new
-  $queue << "" << "" << ""
-
   #############
   # - Limit parallel tasks
+  #############
+  class MultiTask < Task
+    private
+    def invoke_prerequisites(args, invocation_chain)
+      return unless @prerequisites
+      
+      jobqueue = @prerequisites.dup
+      m = Mutex.new
+    
+      numThreads = jobqueue.length > 3 ? 3 : jobqueue.length  
+    
+      threads = [] 
+      numThreads.times {
+        threads << Thread.new(jobqueue) { |jq|
+        	while true do
+	            p = nil
+        		m.synchronize() { p = jq.shift }
+        		break unless p
+        		application[p].invoke_with_call_chain(args, invocation_chain)
+        	end
+        }
+      }
+      threads.each { |t| t.join }
+    end
+  end
+ 
+  
+
+  #############
   # - Go on if a task fails (but to not execute the parent)
   # - showInGraph is used for GraphWriter (internal tasks are not shown)
   #############
   class Task
-    attr_accessor :failure
-    attr_accessor :deps
+    attr_accessor :failure # specified if that task has failed
+    attr_accessor :deps # used to store deps by depfile task for the apply task (no re-read of depsfile)
 
     execute_org = self.instance_method(:execute)
     initialize_org = self.instance_method(:initialize)
@@ -26,7 +52,6 @@ module Rake
     end
 
     define_method(:execute) do |arg|
-
       # check if a prereq has failed
       @prerequisites.each { |n|
         prereq = application[n, @scope]
@@ -36,22 +61,18 @@ module Rake
       }
       break if @failure # if yes, this task cannot be run
 
-      $queue.pop
-
       begin
         execute_org.bind(self).call(arg)
         @failure = false
-      rescue Exception => ex# todo: no rescue to stop on first error
+      rescue Exception => ex1# todo: no rescue to stop on first error
+          $logger.error("#{@name} not built/cleaned correctly: #{ex1.message}")
         begin
-          FileUtils.rm([@name]) # todo: is that the best way? gcc can return errors even if files are built...
-        rescue
+          FileUtils.rm(@name) if File.exists?(@name) # todo: error parsing?
+        rescue Exception => ex2
+          $logger.error("Could not delete #{@name}: #{ex2.message}")
         end
-        $logger.error("#{@name} not built/cleaned correctly")
         @failure = true
       end
-
-      $queue << ""
-
     end
 
   end
