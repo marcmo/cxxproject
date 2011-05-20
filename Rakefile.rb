646b277b845357df8b7d637ca1fd59d64f6a592c
Christian Köstlin <christian.koestlin@gmail.com>
2011-05-20 21:04:21 +0200

    refactored rakefile

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,21 +1,11 @@
-require 'rake/gempackagetask'
-
-
-begin
-  require 'spec/rake/spectask' # old rspec
-rescue LoadError
-  begin
-    require 'rspec/core/rake_task' # rspec 2.5.x
-  rescue LoadError
-    warn "spec not installed...will not be checked!"
-  end
-end
-
 desc "Default Task"
 task :default => [:install]
 
+
+require 'rake/gempackagetask'
 spec = Gem::Specification.load('cxx.gemspec')
 Rake::GemPackageTask.new(spec) {|pkg|}
+
 
 begin
   require 'roodi'
@@ -26,24 +16,26 @@ rescue LoadError # don't bail out when people do not have roodi installed!
   warn "roodi not installed...will not be checked!"
 end
 
-# old rspec
-if self.class.const_defined?(:SpecTask) then
-  desc "Run all examples"
-  Spec::Rake::SpecTask.new() do |t|
-    t.spec_files = FileList['spec/**/*_spec.rb']
-  end
-  task :gem => [:spec]
-end
-
-# new rspec
-begin # const_defined? did not work?
-  desc "Run all examples"
+desc "Run all examples"
+begin
+  SPEC_PATTERN ='spec/**/*_spec.rb' 
+  require 'rspec/core/rake_task'
   RSpec::Core::RakeTask.new() do |t|
-    t.pattern = 'spec/**/*_spec.rb'
+    t.pattern = SPEC_PATTERN
   end
-  task :gem => [:spec]
-rescue
+rescue LoadError
+  begin
+    require 'spec/rake/spectask'
+    Spec::Rake::SpecTask.new() do |t|
+      t.spec_files = SPEC_PATTERN
+    end
+  rescue LoadError
+    task 'spec' do
+      warn 'rspec not installed...will not be checked! please install gem install rspec'
+    end
+  end
 end
+task :gem => [:spec]
 
 desc "install gem globally"
 task :install => [:gem] do
