1b3f9c4c1c6f2a2cb15d1e31d67cdaaf5768c140
alexander schaal <alex_cxxproject@cxxproject.com>
2011-07-18 15:29:11 +0200

    fixed non-ascii char console prob in windows

--- lib/cxxproject/utils/process.rb
+++ lib/cxxproject/utils/process.rb
@@ -13,6 +13,10 @@ module Cxxproject
       Process.wait(sp)
       rd.close
       
+      # seems that pipe cannot handle non-ascii characters right on windows (even with correct encoding)  
+      consoleOutput.gsub!(/\xE2\x80\x98/,"`") # ÔÇÿ
+      consoleOutput.gsub!(/\xE2\x80\x99/,"'") # ÔÇÖ
+      
       consoleOutput
     end
 
