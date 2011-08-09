c0bb4689b087f5e3f6fe43c4f78fa6525b6f844e
alexander schaal <alex_cxxproject@cxxproject.com>
2011-08-09 16:48:21 +0200

    interface spec is running again

--- spec/ide_interface_spec.rb
+++ spec/ide_interface_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require 'cxxproject/ide_interface'
 require 'cxxproject/utils/utils'
+require 'cxxproject/errorparser/error_parser'
 
 def check_long(e, l)
   e.next.should eq(l)
@@ -18,7 +19,11 @@ describe Cxxproject::IDEInterface do
 
   it 'should create a correct package from an error-array' do
     ide = Cxxproject::IDEInterface.new
-    error = ['filename', '10', 2, 'error']
+    error = Cxxproject::ErrorDesc.new
+    error.severity = 2
+    error.line_number = 10
+    error.file_name = 'filename'
+    error.message = 'error'
     packet = ide.create_error_packet(error)
     
     if not Cxxproject::Utils.old_ruby? # in Ruby 1.8.6 there is no bytes methods...    
