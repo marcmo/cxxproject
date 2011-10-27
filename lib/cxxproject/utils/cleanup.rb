bf7e6ffdbb34d127fbc11407ae738c23054dc3f9
alexander schaal <alex_cxxproject@cxxproject.com>
2011-10-27 17:09:11 +0200

    Fixed cleanup method for specs

--- lib/cxxproject/utils/cleanup.rb
+++ lib/cxxproject/utils/cleanup.rb
@@ -8,6 +8,7 @@ module Cxxproject
     def self.cleanup_rake()
       ALL_BUILDING_BLOCKS.clear
       Rake.application.clear
+      Rake.application.idei.set_abort(false)
       CLEAN.pending_add.clear
       CLEAN.items.clear
       task :clean do
