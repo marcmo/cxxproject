352408d8a77e04d9d148199f56e6b4c945aca8e6
alexander schaal <alex_cxxproject@cxxproject.com>
2011-12-22 13:29:09 +0100

    Improved diab linker error parser

--- lib/cxxproject/errorparser/diab_linker_error_parser.rb
+++ lib/cxxproject/errorparser/diab_linker_error_parser.rb
@@ -5,6 +5,7 @@ module Cxxproject
 
     def initialize()
       @error_expression = /dld: ([A-Za-z]+): (.+)/
+      @error_expression_linkerscript = /dld: \"([^\"]+)\", line ([0-9]+): (.+)/
     end
 
     def scan_lines(consoleOutput, proj_dir)
@@ -14,14 +15,21 @@ module Cxxproject
         l.rstrip!
         d = ErrorDesc.new
         scan_res = l.scan(@error_expression)
-        if scan_res.length == 0 # msg will end with the beginning of the next message
+        scan_res2 = l.scan(@error_expression_linkerscript)
+        if scan_res.length == 0 and scan_res2.length == 0 # msg will end with the beginning of the next message
           d.severity = error_severity
           d.message = l
-        else
+        elsif scan_res.length > 0
           d.file_name = proj_dir
           d.line_number = 0
           d.message = scan_res[0][1]
           d.severity = get_severity(scan_res[0][0])
+          error_severity = d.severity
+        else
+          d.file_name = proj_dir+"/"+scan_res2[0][0]
+          d.line_number = scan_res2[0][1].to_i
+          d.message = scan_res2[0][2]
+          d.severity = SEVERITY_ERROR
           error_severity = d.severity
         end
         res << d
--- lib/cxxproject/version.rb
+++ lib/cxxproject/version.rb
@@ -1,6 +1,8 @@
 module Cxxproject
   class Version
     def self.cxxproject
+      "0.5.46"
+      "0.5.47"
       "0.5.48"
     end
   end
