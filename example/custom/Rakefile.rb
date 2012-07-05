e8c4a3464b20f84746ef989c167388f5cb602ff6
Kevin Russ <kevin.russ@esrlabs.com>
2012-07-05 15:04:46 +0200

    fixed custom/big_project examples
    Change-Id: I5c616d516cb2e1a103fbf47bdbcbe27fcc99df58

--- example/custom/Rakefile.rb
+++ example/custom/Rakefile.rb
@@ -2,10 +2,11 @@ $:.unshift File.join(File.dirname(__FILE__),"..","..","lib")
 require 'cxxproject'
 BuildDir = "BuildDir"
 
+dependent_projects =  ['./project.rb']
+CxxProject2Rake.new(dependent_projects, BuildDir, "gcc", './')
+
 unittest_flags = {
   :DEFINES => ['UNIT_TEST','CPPUNIT_MAIN=main'],
   :FLAGS => "-O0 -g3 -Wall"
 }
-toolchain = Provider.modify_cpp_compiler("GCC", unittest_flags)
-dependent_projects =  ['./project.rb']
-CxxProject2Rake.new(dependent_projects, BuildDir, toolchain, './')
+toolchain = Provider.modify_cpp_compiler("gcc", unittest_flags)
