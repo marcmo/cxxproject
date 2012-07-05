ebe222fd19935b1572c28d7032a4e3a8faec9ce5
oliver <oliver.mueller@gmail.com>
2012-07-05 10:44:10 +0200

    added wizard support for toolchains
    Change-Id: I5d1425f3acef7741ff802e269db4e665b603b6b4

--- lib/cxxproject/version.rb
+++ lib/cxxproject/version.rb
@@ -1,7 +1,7 @@
 module Cxxproject
   class Version
     def self.cxxproject
-      "0.6.2"
+      "0.6.4"
     end
   end
 end
--- lib/tools/project_wizard.rb
+++ lib/tools/project_wizard.rb
@@ -1,5 +1,27 @@
 require 'rake'
 require 'erb'
+require 'rubygems'
+
+def choose_toolchain
+  res = nil
+  toolchains = []
+  prefix = "cxxproject_"
+  toolchain_gems = Gem::Specification.find_all do |gem|
+    gem.name.start_with?(prefix)
+  end
+  if toolchain_gems.length > 0
+    choose do |menu|
+      menu.prompt = "choose a toolchain:"
+      toolchain_gems.each do |gem|
+        name = gem.name[prefix.length..-1]
+        menu.choice(name.to_sym) { res = name }
+      end
+    end
+  else
+    say "no toolchains found...you need to install toolchain-gems in order to use cxxproject"
+  end
+  res
+end
 
 def choose_building_block
   res = nil
@@ -27,10 +49,12 @@ def prepare_project(d)
     require 'highline/import'
 
     if agree("This will create a new cxx-project config in dir \"#{d}\" \nAre you sure you want to continue? [yn] ")
+      tc = choose_toolchain
+      return unless tc
       bb = choose_building_block
       generate_makefile = choose_generate_makefile
 
-      create_project(d, bb, generate_makefile)
+      create_project(d, bb, tc, generate_makefile)
     else
       say "stopped project creation"
     end
@@ -40,13 +64,14 @@ def prepare_project(d)
 end
 
 
-def create_project(d, bb, generate_rakefile)
+def create_project(d, bb, tc, generate_rakefile)
   rakefile_template = File.join(File.dirname(__FILE__),"..","tools","Rakefile.rb.template")
   projectrb_template = File.join(File.dirname(__FILE__),"..","tools","project.rb.template")
   s1 = IO.read(rakefile_template)
   s2 = IO.read(projectrb_template)
   name = "testme"
   building_block = bb
+  toolchain_name = tc[/(.*?)toolchain/,1] 
   mkdir_p(d, :verbose => false)
   cd(d,:verbose => false) do
     if ((File.exists? "Rakefile.rb") || (File.exists? "project.rb"))
