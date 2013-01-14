8a52581b376e3d28bf9416bc631409506eb24acb
Christian Köstlin <christian.koestlin@esrlabs.com>
2013-01-14 15:53:42 +0100

    refined deprecated with the help of martin thiede
    Change-Id: If19c74bf31959126473de03efb1689c7346dad32

--- lib/cxxproject/utils/deprecated.rb
+++ lib/cxxproject/utils/deprecated.rb
@@ -1,22 +1,20 @@
 # modified from http://www.seejohncode.com/2012/01/09/deprecating-methods-in-ruby/
 module Deprecated
-  class << self
-    attr_accessor :deprecated_warnings
+  attr_accessor :deprecated_warnings
+
+  def reset
+    self.deprecated_warnings = {}
   end
 
-  def Deprecated.reset
-    Deprecated.deprecated_warnings = {}
-  end
-
-  Deprecated.deprecated_warnings = {}
   # Define a deprecated alias for a method
   # @param [Symbol] name - name of method to define
   # @param [Symbol] replacement - name of method to (alias)
   def deprecated_alias(name, replacement)
     define_method(name) do |*args, &block|
-      if Deprecated.deprecated_warnings.has_key?(name) == false
-        warn "##{name} deprecated (please use ##{replacement})"
-        Deprecated.deprecated_warnings[name] = true
+      self.class.deprecated_warnings ||= {}
+      if self.class.deprecated_warnings.has_key?(name) == false
+        warn "#{self.class.name}##{name} deprecated (please use ##{replacement})"
+        self.class.deprecated_warnings[name] = true
       end
       send replacement, *args, &block
     end
--- spec/deprecated_spec.rb
+++ spec/deprecated_spec.rb
@@ -6,7 +6,8 @@ describe Deprecated do
     @h = $stderr
     @io = StringIO.new
     $stderr = @io
-    Deprecated.reset
+    DeprecatedTest.reset
+    DeprecatedTest2.reset
   end
 
   after (:each) do
@@ -19,15 +20,28 @@ describe Deprecated do
     end
     deprecated_alias :old_method, :new_method
   end
+  class DeprecatedTest2
+    extend Deprecated
+    def new_method
+    end
+    deprecated_alias :old_method, :new_method
+  end
 
   it 'should output deprecated' do
     DeprecatedTest.new.old_method
-    @io.string.should == "#old_method deprecated (please use #new_method)\n"
+    @io.string.should == "DeprecatedTest#old_method deprecated (please use #new_method)\n"
   end
 
   it 'should output deprecated once for each method' do
     DeprecatedTest.new.old_method
     DeprecatedTest.new.old_method
-    @io.string.should == "#old_method deprecated (please use #new_method)\n"
+    @io.string.should == "DeprecatedTest#old_method deprecated (please use #new_method)\n"
   end
+
+  it 'should output deprecated once for each method' do
+    DeprecatedTest.new.old_method
+    DeprecatedTest2.new.old_method
+    @io.string.should == "DeprecatedTest#old_method deprecated (please use #new_method)\nDeprecatedTest2#old_method deprecated (please use #new_method)\n"
+  end
+
 end
