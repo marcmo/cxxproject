cb0935961cbdf74c5985cde94bf2a17913fb32ea
Alexander Schaal <alexander.schaal@esrlabs.com>
2014-03-05 14:34:52 +0100

    Remove invalid characters from compiler output

--- lib/cxxproject/utils/process.rb
+++ lib/cxxproject/utils/process.rb
@@ -24,15 +24,8 @@ module Cxxproject
       Process.wait(sp)
       rd.close
       
-      # seems that pipe cannot handle non-ascii characters right on windows (even with correct encoding)  
-      
-      # seems not to work anymore with Ruby 2.0:
-      # consoleOutput.gsub!(/\xE2\x80\x98/,"`") # ÔÇÿ
-      # consoleOutput.gsub!(/\xE2\x80\x99/,"'") # ÔÇÖ
-      
-      # to be tested:
-      # consoleOutput.encode!('UTF-8',  :invalid => :replace, :undef => :replace, :replace => '')
-      # consoleOutput.encode!('binary', :invalid => :replace, :undef => :replace, :replace => '')
+      consoleOutput.encode!('UTF-8',  :invalid => :replace, :undef => :replace, :replace => '')
+      consoleOutput.encode!('binary', :invalid => :replace, :undef => :replace, :replace => '')
       
       consoleOutput
     end
